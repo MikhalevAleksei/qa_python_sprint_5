075b654a14ff280cae10f73dc8c557ae0d849e4e
Aleksei Mikhalev <118346927+MikhalevAleksei@users.noreply.github.com>
2023-10-29 00:07:56 +0300

    test_registration update

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -5,24 +5,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
-from qa_python_sprint_5 import locators
+import locators
 #
 # class TestRegistration:
 #     def test_registration(self, driver):
 
-driver = webdriver.Chrome()
-driver.get("https://stellarburgers.nomoreparties.site/")
+class TestRegistration:
+    def test_registration(self, driver):
 
-driver.find_element(*REF_REGISTRATION).click()
+        # driver = webdriver.Chrome()
+        # driver.get("https://stellarburgers.nomoreparties.site/")
 
-data_name = "Alex"
-gen_data_email = f'Aleksei_Mikhalev{r.randint(100, 900)}@yandex.ru'
-gen_data_password = r.randint(123456, 654321)
+        driver.find_element(*locators.ConstLocators.REF_REGISTRATION).click()
 
-driver.find_element(*INPUT_NAME).send_keys(data_name)
-driver.find_element(*INPUT_EMAIL).send_keys(gen_data_email)
-driver.find_element(By.NAME, "Пароль").send_keys("gen_data_password")
-driver.find_element(By.XPATH, "//button[text()='Войти']").click()
+        data_name = "Alex"
+        gen_data_email = f'Aleksei_Mikhalev{r.randint(100, 900)}@yandex.ru'
+        gen_data_password = r.randint(123456, 654321)
+
+        driver.find_element(*locators.ConstLocators.INPUT_NAME).send_keys(data_name)
+        driver.find_element(*locators.ConstLocators.INPUT_EMAIL).send_keys(
+            gen_data_email)
+        driver.find_element(By.NAME, "Пароль").send_keys("gen_data_password")
+        driver.find_element(By.XPATH, "//button[text()='Войти']").click()
 
 
-driver.quit()
+        driver.quit()
